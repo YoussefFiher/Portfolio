fc554a7c391c9fe4a46f8a0d6d65e736e232ec29
YoussefFiher <99363422+YoussefFiher@users.noreply.github.com>
2024-11-02 01:59:24 +0100

    first version of my portfolio

---
+++ js/resume.js
@@ -0,0 +1,39 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#sideNav'
+  });
+
+  // Show modal with specified image
+  window.showModal = function(imageSrc) {
+    $('#certificateModal').css('display', 'block');
+    $('#modalImage').attr('src', 'img/' + imageSrc);
+  };
+
+  // Hide modal
+  window.hideModal = function() {
+    $('#certificateModal').css('display', 'none');
+  };
+
+})(jQuery); // End of use strict
